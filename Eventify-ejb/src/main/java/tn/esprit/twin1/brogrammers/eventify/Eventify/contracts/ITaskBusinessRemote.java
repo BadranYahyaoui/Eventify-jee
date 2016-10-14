fd29e07eb86d20032d5526f99bcede42a19f3a5a
badran08 <mohamedbadran.yahyaoui@esprit.tn>
2016-10-14 16:29:30 +0100

    Adding interface ItaskBusinessLocal and ItaskBusinessRemote

---
+++ Eventify-ejb/src/main/java/tn/esprit/twin1/brogrammers/eventify/Eventify/contracts/ITaskBusinessRemote.java
@@ -0,0 +1,40 @@
+package tn.esprit.twin1.brogrammers.eventify.Eventify.contracts;
+
+import java.util.List;
+
+import tn.esprit.twin1.brogrammers.eventify.Eventify.domain.Task;
+
+public interface ITaskBusinessRemote {
+			//update task
+			   public void updateTask(Task task);
+			   
+			   //DeleteTask
+			   public void deleteTask(Task task);
+			
+			//get a task by id
+			public Task findTaskByID(int id);
+			
+			//get all the Tasks Related To an event
+			public List<Task>getAllTasksByID(int idEvent);
+			
+			//get all the tasks Assigned to an oganizer -- get all
+			public  List<Task>GetTasksByOrganizer(int idOrganizer);
+				
+			 //cancel task to an orgnizer
+			public boolean cancelTask(Task task,int idOrganizer);
+			
+			
+			
+			//asssign task to an oganizer
+			public void assignTaskToOrgnizer(int idOrgnizer,int Taskid);
+			
+			 //when Orgnizer complete Task
+				
+			public void taskStatusCompleted(int idOrgnizer,int Taskid);
+			
+			
+			//get a task status --Completed --not completed --Working On
+			public int getTaskStatus(int taskID);
+	
+	
+}
