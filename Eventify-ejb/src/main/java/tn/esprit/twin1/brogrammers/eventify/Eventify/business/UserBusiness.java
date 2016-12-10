0a3b816626abd949c2d7a940014672723cf75b1c
HakimMliki <ovtestovtest@gmail.com>
2016-12-10 16:22:13 +0100

    Token Delai from 60 sec to 5 Min

--- Eventify-ejb/src/main/java/tn/esprit/twin1/brogrammers/eventify/Eventify/business/UserBusiness.java
+++ Eventify-ejb/src/main/java/tn/esprit/twin1/brogrammers/eventify/Eventify/business/UserBusiness.java
@@ -1,5 +1,6 @@
 package tn.esprit.twin1.brogrammers.eventify.Eventify.business;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -38,6 +39,7 @@ public class UserBusiness implements UserBusinessRemote, UserBusinessLocal {
 		user.setAccountState(AccountState.NOTACTIVATED);
 		String activationHashedCode = MD5Hash.getMD5Hash(user.getUsername() + user.getEmail());
 		user.setConfirmationToken(activationHashedCode);
+		user.setCreationDate(new Date());
 		// Emailer.sendEmail("Eventify Account Activation",
 		// "http://localhost:18080/Eventify-web/rest/users/confirm/"+activationHashedCode,
 		// user.getEmail());
@@ -110,7 +112,7 @@ public class UserBusiness implements UserBusinessRemote, UserBusinessLocal {
 		String hashedPwd=MD5Hash.getMD5Hash(pwd);
 		Query query = entityManager.createQuery(
 				"SELECT new User(u.id,u.firstName,u.lastName,u.username,u.profileImage,u.numTel,u.email,u.password,u.creationDate,u.loyaltyPoint,u.accountState,u.confirmationToken) "
-						+ "FROM User u WHERE (u.username=:uname AND u.password=:upwd) ");
+						+ "FROM User u WHERE ( (u.username=:uname OR u.email=:uname) AND u.password=:upwd) ");
 		User userLogged =(User) query.setParameter("uname", username).setParameter("upwd", hashedPwd).getSingleResult();
 		
 		
