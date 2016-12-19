16ea322c16ff37ff6527d17c588c8c6b8f7623fb
MrFirases <mohamedfiras.ouertani@esprit.tn>
2016-12-20 00:11:57 +0100

    Adding some Reservation attributes

--- Eventify-ejb/src/main/java/tn/esprit/twin1/brogrammers/eventify/Eventify/domain/Reservation.java
+++ Eventify-ejb/src/main/java/tn/esprit/twin1/brogrammers/eventify/Eventify/domain/Reservation.java
@@ -34,6 +34,14 @@ public class Reservation implements Serializable {
 	private String address2;
 	private String city;
 	private int postalCode;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+
+
+
 	private static final long serialVersionUID = 1L;
 
 	public Reservation(float amount, Date reservationDate, ReservationState reservationState,
@@ -138,6 +146,38 @@ public class Reservation implements Serializable {
 
 	public void setAmount(float amount) {
 		this.amount = amount;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 
 	public Date getReservationDate() {
